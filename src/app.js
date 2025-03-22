7d769a4f2e0feac76b8b4d0b1216060ed9eaa389
AlenCirikovic <alen.ciriko@gmail.com>
2025-03-22 02:18:06 +0100

    Simple get and post requests with postman

--- src/app.js
+++ src/app.js
@@ -1,2 +1,34 @@
-const {v4 : uuidv4} = require('uuid');
-console.log(uuidv4());
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const customers = [
+    {
+        "name": "Alen",
+        "industry": "Music"
+    },
+    {
+        "name": "John",
+        "industry": "Networking"
+    },
+    {
+        "name": "Marko",
+        "industry": "Sports"
+    }
+];
+
+app.get('/api/customers',(req,res)=>{
+    res.send({"customers": customers});
+})
+
+app.get('/', (req,res) =>{
+    res.send('Welcome!');
+})
+
+app.post('/',(req,res)=>{
+    res.send('This is a post request!');
+})
+
+app.listen(PORT,() => {
+    console.log('App listening on port' + " " + PORT);
+})
