93bc763dc3c4657332243b2e918fe002d06476e8
AlenCirikovic <alen.ciriko@gmail.com>
2025-03-24 19:08:53 +0100

    Added fetch api calls with get calls for specific orders

--- src/models/customer.js
+++ src/models/customer.js
@@ -6,7 +6,13 @@ const customerSchema = new mongoose.Schema({
         type:String,
         required:true
     },
-    industry: String
+    industry: String,
+    orders: [
+        {
+            description: String,
+            amountInCents: Number
+        }
+    ]
 });
 
 module.exports = mongoose.model('Customer',customerSchema); // module exports stands for what we want to export to be used in seperate files over the project. 
